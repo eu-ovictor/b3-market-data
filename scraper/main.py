d5c2a8aaeeba84c0832c7d056097d47a45d4545c
Victor Gonçalves <victorgoncalveslds@proton.me>
2024-07-09 08:59:16 -0300

    Add scraper progress bar

--- scraper/main.py
+++ scraper/main.py
@@ -2,31 +2,30 @@ import aiofiles
 import aiohttp
 import asyncio
 import os
-import zipfile
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+from tqdm import tqdm
 
 BASE_URL = "https://www.b3.com.br/pt_br/market-data-e-indices/servicos-de-dados/market-data/cotacoes/cotacoes/"
 ASSETS_PATH = "./assets/"
 
 
-async def fetch_market_data_file(session, url):
+async def fetch_market_data_file(session, url, pbar):
     async with session.get(url) as response:
         if response.status == 200:
-            file_name = url.split("/")[-1]
+            file_name = url.split("/")[-1] + ".zip"
 
-            zip_file_path = os.path.join("/", "tmp", file_name + ".zip")
+            file_path = os.path.join(ASSETS_PATH, file_name)
 
-            async with aiofiles.open(zip_file_path, "wb") as f:
+            async with aiofiles.open(file_path, "wb") as f:
                 await f.write(await response.content.read())
 
-            with zipfile.ZipFile(zip_file_path, "r") as zip_ref:
-                for file in zip_ref.namelist():
-                    if file.endswith(".txt"):
-                        zip_ref.extract(file, ASSETS_PATH)
+            os.chmod(file_path, 0o777)
+
+            pbar.update(1)
 
 
 async def main():
@@ -50,8 +49,13 @@ async def main():
         if "tickercsv" in link.get_attribute("href")
     ]
 
-    async with aiohttp.ClientSession() as session:
-        await asyncio.gather(*[fetch_market_data_file(session, link) for link in links])
+    with tqdm(
+        total=len(links),
+        desc="Obtendo arquivos de negócios",
+        unit=" arquivos",
+    ) as pbar:
+        async with aiohttp.ClientSession() as session:
+            await asyncio.gather(*[fetch_market_data_file(session, link, pbar) for link in links])
 
 if __name__ == "__main__":
     asyncio.run(main())
