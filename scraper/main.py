ad7e8d6d0f0cded468d9644f8b80f1d870855b64
Victor Gonçalves <victorgoncalveslds@proton.me>
2024-07-09 09:41:13 -0300

    Read scraping parameters from env

--- scraper/main.py
+++ scraper/main.py
@@ -3,14 +3,16 @@ import aiohttp
 import asyncio
 import os
 
+from datetime import date, timedelta
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from tqdm import tqdm
 
-BASE_URL = "https://www.b3.com.br/pt_br/market-data-e-indices/servicos-de-dados/market-data/cotacoes/cotacoes/"
-ASSETS_PATH = "./assets/"
+B3_URL = os.getenv("B3_URL", "https://www.b3.com.br/pt_br/market-data-e-indices/servicos-de-dados/market-data/cotacoes/cotacoes/")
+DOWNLOADS_DIR = os.getenv("DOWLOADS_DIR", "downloads")
+OFFSET = int(os.getenv("OFFSET", 7))
 
 
 async def fetch_market_data_file(session, url, pbar):
@@ -18,7 +20,7 @@ async def fetch_market_data_file(session, url, pbar):
         if response.status == 200:
             file_name = url.split("/")[-1] + ".zip"
 
-            file_path = os.path.join(ASSETS_PATH, file_name)
+            file_path = os.path.join(os.getcwd(), DOWNLOADS_DIR, file_name)
 
             async with aiofiles.open(file_path, "wb") as f:
                 await f.write(await response.content.read())
@@ -37,17 +39,28 @@ async def main():
 
     driver = webdriver.Chrome(service=Service(), options=chrome_options)
 
-    driver.get(BASE_URL)
+    driver.get(B3_URL)
 
     iframe = driver.find_element(By.TAG_NAME, "iframe")
 
     driver.switch_to.frame(iframe)
 
-    links = [
-        link.get_attribute("href")
-        for link in driver.find_elements(By.TAG_NAME, "a")
-        if "tickercsv" in link.get_attribute("href")
-    ]
+    dates = []
+
+    for i in range(1, 8):
+        dt = date.today() - timedelta(days=i)
+        dates.append(dt.strftime('%Y-%m-%d'))
+
+    links = []
+
+    for link in driver.find_elements(By.TAG_NAME, "a"):
+        href = link.get_attribute("href")
+
+        if href and "tickercsv" in href:
+            reference_date = href.split("/")[-1]
+
+            if reference_date in dates:
+                links.append(href)
 
     with tqdm(
         total=len(links),
